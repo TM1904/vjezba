57a98c3ebf88e268ebcceefd6175e2966a499588
Tomo <56563575+TM1904@users.noreply.github.com>
2021-01-03 21:27:39 +0100

    Add files via upload

---
+++ lineweb.js
@@ -0,0 +1,62 @@
+
+const fun = ()=>{
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btn = document.getElementById("button");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btn.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btn.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+const fun1 = ()=>{
+    var dotsD = document.getElementById("dots1");
+    var moreTextM = document.getElementById("more1");
+    var btnn = document.getElementById("button1");
+
+    if (dotsD.style.display === "none") {
+        dotsD.style.display = "inline";
+        btnn.innerHTML = "Read more";
+        moreTextM.style.display = "none";
+    } else {
+        dotsD.style.display = "none";
+        btnn.innerHTML = "Read less";
+        moreTextM.style.display = "inline";
+    }
+}
+const fun2 = ()=>{
+    var dotsDD = document.getElementById("dots2");
+    var moreTextMM = document.getElementById("more2");
+    var btnnn = document.getElementById("button2");
+
+    if (dotsDD.style.display === "none") {
+        dotsDD.style.display = "inline";
+        btnnn.innerHTML = "Read more";
+        moreTextMM.style.display = "none";
+    } else {
+        dotsDD.style.display = "none";
+        btnnn.innerHTML = "Read less";
+        moreTextMM.style.display = "inline";
+    }
+}
+
+const fun3 = ()=>{
+    var dotsDDD = document.getElementById("dots3");
+    var moreTextMMM = document.getElementById("more3");
+    var btnnnn = document.getElementById("button3");
+
+    if (dotsDDD.style.display === "none") {
+        dotsDDD.style.display = "inline";
+        btnnnn.innerHTML = "Read more";
+        moreTextMMM.style.display = "none";
+    } else {
+        dotsDDD.style.display = "none";
+        btnnnn.innerHTML = "Read less";
+        moreTextMMM.style.display = "inline";
+    }
+}
